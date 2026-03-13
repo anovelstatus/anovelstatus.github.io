7b199f720b874a5ee18e74475f891e7b871f5719
A Novel Status <anovelstatus@gmail.com>
2026-03-13 18:14:18 -0400

    Use for API

--- api/build.ts
+++ api/build.ts
@@ -1,24 +1,33 @@
 // https://manikumar.in/blog/integrating-npm-packages-in-google-apps-script-guide/
-import esbuild from "esbuild";
+import { build } from "rolldown";
+import { replacePlugin } from "rolldown/plugins";
 import "dotenv/config";
+import { copyFile } from "fs";
+import { promisify } from "util";
 
-const banner = `
+await build({
+	input: ["src/index.ts"],
+	output: {
+		dir: "dist/",
+		format: "esm",
+		banner: `
 // Scroll all the way down to see the entry points you can debug with.
 // The code here is compiled and deployed from https://github.com/anovelstatus/anovelstatus.github.io
-/* eslint-disable no-undef */`;
-
-await esbuild.build({
-	entryPoints: ["src/index.ts", "src/appsscript.json"],
-	bundle: true,
-	outdir: "dist/",
-	format: "esm",
-	treeShaking: false,
-	minifyWhitespace: false,
-	minifyIdentifiers: false,
-	banner: { js: banner },
-	loader: { ".json": "copy" },
-	define: {
-		PATREON_KEY: `"${process.env["PATREON_KEY"] || "testing"}"`,
-		SS_LINK: `"${process.env["SS_LINK"] || "testing"}"`,
+/* eslint-disable no-undef */`,
+		minify: false,
 	},
+	treeshake: false,
+	plugins: [
+		replacePlugin(
+			{
+				PATREON_KEY: `"${process.env["PATREON_KEY"] || "testing"}"`,
+				SS_LINK: `"${process.env["SS_LINK"] || "testing"}"`,
+			},
+			{ preventAssignment: true },
+		),
+	],
 });
+
+const asyncCopyFile = promisify(copyFile);
+
+await asyncCopyFile("src/appsscript.json", "dist/appsscript.json");
