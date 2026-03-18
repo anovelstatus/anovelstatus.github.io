8a079fd41d87d0799f845768e4b45bb39d7cfc4f
A Novel Status <anovelstatus@gmail.com>
2026-03-17 20:28:22 -0400

    fix issue with not found item

--- website/src/data/helpers.ts
+++ website/src/data/helpers.ts
@@ -22,16 +22,21 @@ export const toIdString = (item?: TieredId) => (!item ? "?" : item.name + " - " 
 
 export const sameId = <T extends TieredId>(a: T, b: T) => a.name == b.name && a.tier == b.tier;
 
-export function findByIds<T extends TieredId>(talents: T[], previous?: TieredId[]): T[] {
+export function findByIds<T extends TieredId>(list: T[], previous?: TieredId[]): T[] {
 	if (!previous) return [];
-	return previous.map((x): T => {
-		let match = talents.find((t) => t.name == x.name && t.tier == x.tier);
-		// special search for growth talents
-		if (!match) {
-			match = talents.find((t) => t.name == x.name && x.tier.startsWith(t.tier));
-		}
-		return match as T;
-	});
+	return (
+		previous
+			.map((x): T | undefined => {
+				let match = list.find((t) => t.name == x.name && t.tier == x.tier);
+				// special search for growth talents
+				if (!match) {
+					match = list.find((t) => t.name == x.name && x.tier.startsWith(t.tier));
+				}
+				return match;
+			})
+			// Filter out any that we couldn't find because the chapter is too low for all linked items
+			.filter((x): x is T => !!x)
+	);
 }
 
 export function mapMapValues<TKey, TValue, TNewValue>(
