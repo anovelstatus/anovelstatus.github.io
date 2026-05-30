490d9b8b85b22137df66cdcb20c642f0e005b956
A Novel Status <anovelstatus@gmail.com>
2026-05-30 16:58:40 -0400

    shortcuts

--- api/src/pages/shortcuts.ts
+++ api/src/pages/shortcuts.ts
@@ -1,27 +1,16 @@
-import { chapterFilter, parseNumber, parseOptional, parseString, parseTable } from "./shared";
+import { chapterFilter, parseDynamicTable } from "./shared";
 
-type Columns = Record<keyof Shortcut, number>;
-
-export const getTimelineShortcuts: StandardParser<Shortcut[]> = ({ ss, ranges, chapterLimit }) => {
-	const range = ss.getRange(ranges["Chapter Shortcuts"]);
-
-	return parseTable(range, mapColumns, mapRow, chapterFilter(chapterLimit, "chapter"));
-};
-
-function mapColumns(headerRow: SpreadsheetValue[]): Columns {
-	return {
-		chapter: headerRow.indexOf("Chapter"),
-		label: headerRow.indexOf("Label"),
-		group: headerRow.indexOf("Group"),
-		menu: headerRow.indexOf("Menu"),
+export function getTimelineShortcuts(info: SpreadsheetInfo) {
+	const definition: Table<Shortcut> = {
+		getRange: (info) => info.ss.getRange(info.ranges["Chapter Shortcuts"]),
+		filter: chapterFilter(info.chapterLimit, "chapter"),
+		fields: [
+			{ key: "chapter", source: { type: "column", name: "Chapter" }, parse: { type: "number" } },
+			{ key: "label", source: { type: "column", name: "Label" }, parse: { type: "string" } },
+			{ key: "group", source: { type: "column", name: "Group" }, parse: { type: "string", optional: true } },
+			{ key: "menu", source: { type: "column", name: "Menu" }, parse: { type: "string", optional: true } },
+		],
+		extra: undefined,
 	};
-}
-
-function mapRow(row: SpreadsheetValue[], headers: Columns): Shortcut {
-	return {
-		chapter: parseNumber(row[headers.chapter]),
-		label: parseString(row[headers.label]),
-		group: parseOptional<string>(row[headers.group]),
-		menu: parseOptional<string>(row[headers.menu]),
-	};
+	return parseDynamicTable(info, definition);
 }
