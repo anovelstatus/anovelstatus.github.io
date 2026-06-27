c318a2406b254b7c6e3f90bc0d9be32920dfb5b8
A Novel Status <anovelstatus@gmail.com>
2026-06-27 11:32:53 -0400

    cell styling by tier

--- website/src/features/titles/merits/columns.tsx
+++ website/src/features/titles/merits/columns.tsx
@@ -1,6 +1,13 @@
-import { Stack, Typography, type SxProps, type Theme } from "@mui/material";
+import { Box, Stack, Typography, type SxProps, type Theme } from "@mui/material";
 import { ChaptersChip, RarityChip } from "@/components/chips";
-import { createColumnHelper, type ColumnDef } from "@tanstack/react-table";
+import {
+	createColumnHelper,
+	type Cell,
+	type ColumnDef,
+	type Header,
+	type Row,
+	type Table,
+} from "@tanstack/react-table";
 import { createCollapsedTierColumn } from "@/components/AppTable/columns";
 import { useChapter, useMetalTiers, useTitles } from "@/data/api";
 import { RichTextSpan } from "@/components/RichTextSpan";
@@ -8,6 +15,8 @@ import { WrappedRow } from "@/components/WrappedRow";
 import { useMemo } from "react";
 import { getPreviousTitleChain } from "../helpers";
 import { maxBy } from "es-toolkit";
+import { useTheme } from "@/data/useTheme";
+import { sameId, toIdString } from "@/data/helpers";
 
 export const columnstyles: SxProps<Theme> = {
 	".bought": {
@@ -36,20 +45,37 @@ export const useColumns = () => {
 				<WrappedRow sx={{ paddingLeft: `${row.depth}rem` }}>
 					<Typography variant="subtitle1">{row.original.name}</Typography>
 					<RarityChip name={row.original.tier} />
+					{row.original.noTreeReason && (
+						<>
+							{" - "}
+							<RichTextSpan data={row.original.noTreeReason} />
+						</>
+					)}
 				</WrappedRow>
 			),
 			meta: {
-				bodyColSpan: 2,
+				bodyColSpan: (row) => {
+					if (row.original.noTreeReason) return 12;
+					return 2;
+				},
 			},
 		},
 		createCollapsedTierColumn<Title>(metalTiers),
 	] as ColumnDef<Title>[];
 
 	for (let i = 0; i < 10; i++) {
+		const columnTierNumber = Math.max(0, i - 2);
+		const columnTier = metalTiers[columnTierNumber]!;
+		const tierTheme = useTheme(columnTierNumber);
 		columns.push(
 			columnHelper.display({
 				id: "tier-" + i,
-				header: "Tier " + i,
+				header: (context) => (
+					<Stack>
+						<Box>Tier {i}</Box>
+						<Box sx={{ fontSize: "0.95em" }}>Requires {columnTier} Title</Box>
+					</Stack>
+				),
 				size: 200,
 				cell: ({ row }) => {
 					const chain = useTitleChain(row.original);
@@ -65,6 +91,9 @@ export const useColumns = () => {
 					);
 				},
 				meta: {
+					bodyColSpan: (row) => {
+						return row.original.noTreeReason ? 0 : 1;
+					},
 					bodyClassName: (cell): string => {
 						const chain = useTitleChain(cell.row.original);
 						const merit = getMerit(chain, i, chapter);
@@ -74,7 +103,33 @@ export const useColumns = () => {
 						}
 						return "";
 					},
-					headerSx: { textAlign: "center" },
+					bodySx: (cell: Cell<Title, unknown>, table: Table<Title>): SxProps => {
+						const style: SxProps = {
+							backgroundColor: tierTheme.palette.primary.dark,
+							//borderTopColor: tierTheme.palette.primary.main,
+							//borderBottomColor: tierTheme.palette.primary.main,
+						};
+						const titleTier = metalTiers.indexOf(cell.row.original.tier);
+						if (titleTier + 1 == columnTierNumber) {
+							style.borderLeftColor = "rgb(182, 0, 0)";
+							style.borderLeftWidth = 4;
+
+							const previousRow = getPreviousRow(cell.row, table);
+							if (previousRow && previousRow.original.tier !== cell.row.original.tier) {
+								style.borderTopColor = "rgb(182, 0, 0)";
+								style.borderTopWidth = 4;
+							}
+						}
+						return style;
+					},
+					headerSx: (column: Header<Title, unknown>): SxProps => {
+						return {
+							backgroundColor: tierTheme.palette.primary.dark,
+							//borderTopColor: tierTheme.palette.primary.main,
+							borderBottomColor: tierTheme.palette.primary.main,
+							textAlign: "center",
+						};
+					},
 				},
 			}),
 		);
@@ -102,4 +157,10 @@ function getMerit(chain: Title[], meritTier: number, chapter: number): TitleMeri
 		return maxBy(titleMerits, (x) => x.chReveal);
 	}
 	return;
+}
+
+function getPreviousRow<T>(row: Row<T>, table: Table<T>) {
+	const allRows = table.getRowModel().rows;
+	const index = allRows.findIndex((x) => x.id == row.id);
+	return allRows[index - 1];
 }
